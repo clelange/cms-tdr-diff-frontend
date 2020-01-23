e24752ef7d9bbb15f461606a5a68dcfaf656dcb5
Clemens Lange <clemens.lange@cern.ch>
2020-01-24 00:54:17 +0100

    Footer cosmetics

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,9 +3,9 @@
     <div class="content has-text-centered">
       <p>
         <strong>CMS TDR Diff</strong> by
-        <a href="https://cern.ch/clange">Clemens Lange</a>
-        . Version hash {{ this.$env.BUILD_HASH }}. Please report issues and feature requests in the
-        <a href="https://github.com/clelange/cms-tdr-diff/issues">GitHub repository</a>.
+        <a href="https://cern.ch/clange">Clemens Lange</a>.
+        Version hash {{ this.$env.BUILD_HASH || "undefined" }}. Please report issues and feature requests in the
+        <a href="https://github.com/clelange/cms-tdr-diff-frontend/issues">GitHub repository</a>.
       </p>
     </div>
   </footer>
