bf70790cdaf4972da6226850927ff81ff8e1e08a
Clemens Lange <clemens.lange@cern.ch>
2020-02-10 22:31:58 +0100

    Update about page

--- pages/About.vue
+++ pages/About.vue
@@ -1,38 +1,58 @@
 <template>
   <div>
-    <div>
-      <page-header />
-    </div>
-    <div class="container">
-      <div>
-        <h1 class="subtitle">Using this website</h1>
-        <p>This website uses the GitLab API at CERN to query existing documents
-          (papers, reports, and notes) as well as their commit history.</p>
-        <p>Start by choosing one of the available categories from the top left
+    <page-header />
+    <section class="hero">
+      <div class="hero-body">
+        <h1 class="title">Using this website</h1>
+        <p>
+          This website uses the GitLab API at CERN to query existing documents
+          (papers, reports, and notes) as well as their commit history.
+        </p>
+
+        <p>
+          Start by choosing one of the available categories from the top left
           menu, then pick the document of your interest.
           You can filter the table entries by typing a part of the document name,
-          e.g. <i>18-002</i> will show B2G-18-002, BPH-18-002, etc. if available
-          in the chosen category.</p>
-        <p>By clicking on the document name, you will see a page that displays
+          e.g.
+          <b>18-002</b> will show B2G-18-002, BPH-18-002, etc. if available
+          in the chosen category.
+        </p>
+        <p>
+          By clicking on the document name, you will see a page that displays
           all commits including their corresponding tags.
           Select two commits, and hit submit. This will trigger a job in GitLab to generate the documents for the two chosen commits as well as the difference between them.
           Creating the three PDF files usually takes a few minutes.
           You can follow the progress on the
-          <nuxt-link to="/StatusBoard">Status Board</nuxt-link>
-          linked from the top right of the page.
+          <nuxt-link to="/StatusBoard">Status Board</nuxt-link> linked from the top right of the page.
           Once the job has completed, a link will appear on the
-          <nuxt-link to="/StatusBoard">Status Board</nuxt-link>
-          that will allow you to download a zip file of the PDF files.
+          <nuxt-link to="/StatusBoard">Status Board</nuxt-link> that will allow you to download a zip file of the PDF files.
         </p>
-        <hr>
-        <h1 class="subtitle">About this project</h1>
-        <p>This project has been developed by Clemens Lange.</p>
-        <p>Please let me know what you think about it: Drop me an email,
-          or if you really like it, <a href="https://ko-fi.com/clange">buy me a coffee</a>!</p>
       </div>
-    </div>
-      <page-footer />
-    </div>
+    </section>
+    <section class="hero">
+      <div class="hero-body">
+        <h1 class="title">About this project</h1>
+        <p>This project has been developed by Clemens Lange.</p>
+        <p>
+          Please let me know what you think about it: Drop me an email,
+          or if you really like it,
+          <a
+            href="https://ko-fi.com/L4L11EBJZ"
+          >buy me a coffee</a>!
+          <br />
+          <a href="https://ko-fi.com/L4L11EBJZ" target="_blank">
+            <img
+              height="36"
+              style="border:0px;height:36px;"
+              src="https://az743702.vo.msecnd.net/cdn/kofi2.png?v=2"
+              border="0"
+              alt="Buy Me a Coffee at ko-fi.com"
+            />
+          </a>
+        </p>
+      </div>
+    </section>
+    <page-footer />
   </div>
 </template>
 
