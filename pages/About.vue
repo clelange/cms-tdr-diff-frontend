27a47ab026547621eaa078750c404a684240d2d0
Clemens Lange <clemens.lange@cern.ch>
2020-05-06 21:03:49 +0200

    Update About page with more instructions and new coffee link

--- pages/About.vue
+++ pages/About.vue
@@ -11,21 +11,28 @@
 
         <p>
           Start by choosing one of the available categories from the top left
-          menu, then pick the document of your interest.
-          You can filter the table entries by typing a part of the document name,
-          e.g.
-          <b>18-002</b> will show B2G-18-002, BPH-18-002, etc. if available
-          in the chosen category.
+          menu, then pick the document of your interest. You can filter the
+          table entries by typing a part of the document name, e.g.
+          <b>18-002</b> will show B2G-18-002, BPH-18-002, etc. if available in
+          the chosen category.
         </p>
         <p>
           By clicking on the document name, you will see a page that displays
-          all commits including their corresponding tags.
-          Select two commits, and hit submit. This will trigger a job in GitLab to generate the documents for the two chosen commits as well as the difference between them.
-          Creating the three PDF files usually takes a few minutes.
-          You can follow the progress on the
-          <nuxt-link to="/StatusBoard">Status Board</nuxt-link> linked from the top right of the page.
-          Once the job has completed, a link will appear on the
-          <nuxt-link to="/StatusBoard">Status Board</nuxt-link> that will allow you to download a zip file of the PDF files.
+          all commits including their corresponding tags. Select two commits,
+          and hit submit. This will trigger a job in GitLab to generate the
+          documents for the two chosen commits as well as the difference between
+          them. Creating the three PDF files usually takes a few minutes. You
+          can follow the progress on the
+          <nuxt-link to="/StatusBoard">Status Board</nuxt-link> linked from the
+          top right of the page. Once the job has completed, a link will appear
+          on the <nuxt-link to="/StatusBoard">Status Board</nuxt-link> that will
+          allow you to download a zip file of the PDF files.
+        </p>
+        <p>
+          Please mind that parsing LateX programmatically is difficult and building
+          a diff is not always possible. In particular, if the TeX files produce
+          errors for one or both of the two chosen commits, the diff will most
+          likely not work either.
         </p>
       </div>
     </section>
@@ -34,21 +41,23 @@
         <h1 class="title">About this project</h1>
         <p>This project has been developed by Clemens Lange.</p>
         <p>
-          Please let me know what you think about it: Drop me an email,
-          or if you really like it,
-          <a
-            href="https://ko-fi.com/L4L11EBJZ"
-          >buy me a coffee</a>!
-          <br />
-          <a href="https://ko-fi.com/L4L11EBJZ" target="_blank">
-            <img
-              height="36"
-              style="border:0px;height:36px;"
-              src="https://az743702.vo.msecnd.net/cdn/kofi2.png?v=2"
-              border="0"
-              alt="Buy Me a Coffee at ko-fi.com"
-            />
-          </a>
+          Please let me know what you think about it: Drop me an email, or if
+          you really like it,
+          <a href="https://www.buymeacoffee.com/clange">buy me a tea</a>!
+          <br /><link
+            href="https://fonts.googleapis.com/css?family=Cookie"
+            rel="stylesheet"
+          /><a
+            class="bmc-button"
+            target="_blank"
+            href="https://www.buymeacoffee.com/clange"
+            ><img
+              src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+              alt="Buy me a tea"
+            /><span style="margin-left:15px;font-size:28px !important;"
+              >Buy me a tea</span
+            ></a
+          >
         </p>
       </div>
     </section>
@@ -102,5 +111,49 @@ export default {
 
 .links {
   padding-top: 15px;
+}
+
+.bmc-button img {
+  height: 34px !important;
+  width: 35px !important;
+  margin-bottom: 1px !important;
+  box-shadow: none !important;
+  border: none !important;
+  vertical-align: middle !important;
+}
+.bmc-button {
+  padding: 7px 10px 7px 10px !important;
+  line-height: 35px !important;
+  height: 51px !important;
+  min-width: 217px !important;
+  text-decoration: none !important;
+  display: inline-flex !important;
+  color: #ffffff !important;
+  background-color: #ff813f !important;
+  border-radius: 5px !important;
+  border: 1px solid transparent !important;
+  padding: 7px 10px 7px 10px !important;
+  font-size: 22px !important;
+  letter-spacing: 0.6px !important;
+  box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;
+  -webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;
+  margin: 0 auto !important;
+  font-family: 'Cookie', cursive !important;
+  -webkit-box-sizing: border-box !important;
+  box-sizing: border-box !important;
+  -o-transition: 0.3s all linear !important;
+  -webkit-transition: 0.3s all linear !important;
+  -moz-transition: 0.3s all linear !important;
+  -ms-transition: 0.3s all linear !important;
+  transition: 0.3s all linear !important;
+}
+.bmc-button:hover,
+.bmc-button:active,
+.bmc-button:focus {
+  -webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;
+  text-decoration: none !important;
+  box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;
+  opacity: 0.85 !important;
+  color: #ffffff !important;
 }
 </style>
